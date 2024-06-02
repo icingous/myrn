8f189dbc11e51ace80193c437fa7df8a3e1627fb
vmikov <vmikov@ukr.net>
2024-06-02 09:52:57 +0300

    hw-9: додано LayoutAnimation для хедера та Animated для EmptyCard

--- src/screen/cart/components/EmptyCart.jsx
+++ src/screen/cart/components/EmptyCart.jsx
@@ -1,10 +1,70 @@
-import { useContext } from "react";
-import { StyleSheet, Text, Pressable, View } from "react-native";
+import { useContext, useMemo, useState } from "react";
+import { Animated, StyleSheet, Text, Pressable, View } from "react-native";
 import { colors } from "../../../constants/colors";
 import ColorSchemeContext from "../../../context/colorThemeContext";
 
+const animationDuration = 500;
+
 const EmptyCart = ({ toTours }) => {
   const { isSchemeLight } = useContext(ColorSchemeContext);
+  const textOffset = useState(new Animated.Value(0))[0];
+  const textOpacity = useState(new Animated.Value(1))[0];
+  const buttonOffset = useState(new Animated.Value(0))[0];
+  const buttonOpacity = useState(new Animated.Value(1))[0];
+
+  const a1 = useMemo(
+    () =>
+      Animated.timing(textOffset, {
+        toValue: -1000,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }),
+    []
+  );
+
+  const a2 = useMemo(
+    () =>
+      Animated.timing(textOpacity, {
+        toValue: 0,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }),
+    []
+  );
+
+  const a3 = useMemo(
+    () =>
+      Animated.timing(buttonOffset, {
+        toValue: 1000,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }),
+    []
+  );
+
+  const a4 = useMemo(
+    () =>
+      Animated.timing(buttonOpacity, {
+        toValue: 0,
+        duration: animationDuration,
+        useNativeDriver: true,
+      }),
+    []
+  );
+
+  const toTours2 = () => {
+    a1.start();
+    a2.start();
+    a3.start();
+    a4.start();
+    setTimeout(() => {
+      toTours();
+      a1.reset();
+      a2.reset();
+      a3.reset();
+      a4.reset();
+    }, 400);
+  };
 
   return (
     <View
@@ -13,17 +73,28 @@ const EmptyCart = ({ toTours }) => {
         isSchemeLight ? styles.containerLight : styles.containerDark,
       ]}
     >
-      <Text
+      <Animated.Text
         style={[
           styles.header,
           isSchemeLight ? styles.headerLight : styles.headerDark,
+          { transform: [{ translateX: textOffset }], opacity: textOpacity },
         ]}
       >
         Cart is empty
-      </Text>
-      <Pressable style={[styles.button, styles.buttonBack]} onPress={toTours}>
-        <Text style={styles.textStyle}>To Tours</Text>
-      </Pressable>
+      </Animated.Text>
+      <Animated.View
+        style={{
+          transform: [{ translateX: buttonOffset }],
+          opacity: buttonOpacity,
+        }}
+      >
+        <Pressable
+          style={[styles.button, styles.buttonBack]}
+          onPress={toTours2}
+        >
+          <Text style={styles.textStyle}>To Tours</Text>
+        </Pressable>
+      </Animated.View>
     </View>
   );
 };
--- src/screen/home/components/CardList.jsx
+++ src/screen/home/components/CardList.jsx
@@ -1,10 +1,40 @@
-import { FlatList, RefreshControl } from "react-native";
+import { useCallback } from "react";
+import { FlatList, LayoutAnimation, RefreshControl } from "react-native";
+import { observer } from "mobx-react-lite";
 import Card from "./CardListItem";
+import store from "../../../store";
 
 const renderItem = ({ item }) => <Card data={item} />;
 const keyExtractor = (item) => item.id;
+const animationConfig = {
+  duration: 500,
+  create: { type: "easeIn", property: "opacity" },
+  update: { type: "easeIn" },
+  delete: { type: "easeIn", property: "opacity" },
+};
 
 const CardList = ({ items, onEndReached, refreshing, onRefresh }) => {
+  const { tours } = store;
+
+  const onScroll = useCallback(
+    (e) => {
+      const { configureNext } = LayoutAnimation;
+
+      if (e.nativeEvent.contentOffset.y > 100) {
+        if (tours.isHeaderVisible) {
+          tours.setHeaderVisible(false);
+          configureNext(animationConfig);
+        }
+      } else {
+        if (!tours.isHeaderVisible) {
+          tours.setHeaderVisible(true);
+          configureNext(animationConfig);
+        }
+      }
+    },
+    [tours]
+  );
+
   return (
     <FlatList
       data={items}
@@ -16,8 +46,9 @@ const CardList = ({ items, onEndReached, refreshing, onRefresh }) => {
       refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
       }
+      onScroll={onScroll}
     />
   );
 };
 
-export default CardList;
+export default observer(CardList);
